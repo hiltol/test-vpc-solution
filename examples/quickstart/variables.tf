1b6fb50de08ba40b8922d36023548068afc44c8a
hlem@ca.ibm.com <hlem@ca.ibm.com>
2023-03-22 15:34:40 -0400

    seed solution from ibm cloud terraform module for SLZ

---
+++ examples/quickstart/variables.tf
@@ -0,0 +1,64 @@
+variable "ibmcloud_api_key" {
+  description = "APIkey that's associated with the account to provision resources to"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "The region to which to deploy the VPC"
+  type        = string
+  default     = "us-south"
+}
+
+variable "prefix" {
+  description = "The prefix that you would like to append to your resources"
+  type        = string
+  default     = "sol-vpc"
+}
+
+variable "name" {
+  description = "The name the VPC will be created with"
+  type        = string
+  default     = "solution"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
+  default     = null
+}
+
+variable "resource_tags" {
+  description = "List of Tags for the resource created"
+  type        = list(string)
+  default     = null
+}
+
+variable "machine_type" {
+  description = "VSI machine type"
+  type        = string
+  default     = "cx2-2x4"
+}
+
+variable "image_id" {
+  description = "Image ID used for VSI. Run 'ibmcloud is images' to find available images. Be aware that region is important for the image since the id's are different in each region."
+  type        = string
+  default     = "r134-ab47c72d-b11c-417b-a442-9f1ca6a6f5ed"
+}
+
+variable "ssh_key_id" {
+  type        = string
+  description = "An existing ssh key id to use"
+}
+
+variable "vsi_floating_ip" {
+  description = "Add floating IP to VSIs"
+  type        = bool
+  default     = true
+}
+
+variable "vsi_per_subnet" {
+  description = "Number of VSI instances for each subnet"
+  type        = number
+  default     = 1
+}
---
+++ examples/standard/version.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "1.51.0"
+    }
+
+    catalog = {
+      source = "JonWoodlief/catalog"
+      version = "3.0.1"
+    }
+  }
+}
